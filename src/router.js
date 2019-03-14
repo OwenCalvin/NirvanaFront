2f844993f84308e45e2a5a97056c6acbc3d645bb
Owen Gombas <owen.gombas@gmail.com>
2019-03-14 19:53:05 +0100

    404

--- src/router.js
+++ src/router.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/Home'
 import About from './pages/About'
+import Error404 from './pages/404'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '*',
+      name: '404',
+      component: Error404
+    },
     {
       path: '/',
       name: 'home',
